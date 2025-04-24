823ee21e5a516ec04583d3c87726ae406d82893b
Gonzalo <huntymonster@gmail.com>
2025-04-24 21:17:56 +0200

    a few changes

---
+++ components/form/submit-button.tsx
@@ -0,0 +1,22 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "../ui/button";
+
+export const SubmitButton = ({
+  children,
+  className,
+  disabled,
+  loading,
+  ...props
+}: {
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  loading?: boolean;
+}) => {
+  return (
+    <Button className="bg-primary hover:bg-primary/90">
+      {loading ? <Loader2 className="h-4 w-4 mr-2 animate-spin" /> : null}
+      {children}
+    </Button>
+  );
+};
