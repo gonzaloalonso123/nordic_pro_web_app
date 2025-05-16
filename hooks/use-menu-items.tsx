da3a30abe4292d301e61585a40bd089cf99c38c9
Gonzalo <huntymonster@gmail.com>
2025-05-16 11:25:51 +0200

    fixed navigation sidebar with right routes

--- hooks/use-menu-items.tsx
+++ hooks/use-menu-items.tsx
@@ -11,46 +11,57 @@ import { useRole } from "@/app/app/(role-provider)/role-provider";
 
 const root = flags.current_app;
 
-const generalTeamMenuItems = [
+const generalTeamMenuItems = (root: string) => [
   { name: "Calendar", href: `${root}/calendar`, icon: Calendar },
   { name: "Messages", href: `${root}/chat`, icon: MessageSquare },
 ];
 
-const coachMenuItems = [
+const coachMenuItems = (root: string) => [
   { name: "Management", href: `${root}/management`, icon: Settings },
   { name: "Analytics", href: `${root}/analytics`, icon: BarChart3 },
   { name: "Forms", href: `${root}/forms`, icon: MessageSquare },
 ];
 
-const userMenuItems = [
+const userMenuItems = (root: string) => [
   { name: "Dashboard", href: `${root}/dashboard`, icon: TrophyIcon },
 ];
 
-const adminMenuItems = [
+const adminMenuItems = (root: string) => [
   { name: "Admin", href: `${root}/admin`, icon: UserRoundCog },
 ];
 
-const organisationMenuItems = [
-  { name: "Organisation", href: `${root}/organisation`, icon: Settings },
+const organisationMenuItems = (root: string, orgId: string) => [
+  {
+    name: "Organisation",
+    href: `${root}/organisation/${orgId}`,
+    icon: Settings,
+  },
 ];
 
 export const useMenuItems = () => {
   const { app, organisation, team } = useRole();
 
+  const rootUrl = root;
+
   if (app.role === "ADMIN") {
-    return adminMenuItems;
+    return adminMenuItems(rootUrl);
   }
 
   if (organisation.role === "MANAGER") {
-    return organisationMenuItems;
+    return organisationMenuItems(root, organisation.id);
   }
 
   const isCoach = team.role === "COACH";
   const isUser = team.role === "PLAYER";
 
+  const teamRoot = `${rootUrl}/team/${team.id}`;
+  const generalItems = generalTeamMenuItems(teamRoot);
+  const coachItems = coachMenuItems(teamRoot);
+  const userItems = userMenuItems(teamRoot);
+
   const combinedMenuItems = [
-    ...(isCoach ? coachMenuItems : isUser ? userMenuItems : []),
-    ...generalTeamMenuItems,
+    ...(isCoach ? coachItems : isUser ? userItems : []),
+    ...generalItems,
   ];
 
   return combinedMenuItems;
